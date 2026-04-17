f17a3861ef90f475c9ff7c356be665e33996b60f
Bruno Bortoleto <brunbortoletodev@gmail.com>
2026-04-17 17:43:15 -0300

    Adicionar arquivos de projeto.

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,37 @@
+﻿using CityInfo.API.Models;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDTO> Cities { get; set; }
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDTO>()
+            {
+                new CityDTO
+                {
+                    Id = 1,
+                    Name = "New York City",
+                    Description = "The one with that big park"
+                },
+
+                new CityDTO
+                {
+                    Id = 2,
+                    Name = "Antwerp",
+                },
+
+                new CityDTO
+                {
+                    Id = 3,
+                    Name = "Paris",
+                    Description = "The one with that big tower"
+                }
+            };
+
+        }
+    }
+}
---
+++ CityInfo.API/Models/CityDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace CityInfo.API.Models
+{
+    public class CityDTO
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+
+    }
+}
